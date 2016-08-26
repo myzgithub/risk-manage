7c6d3576b6cccd03705f3a0c93e6ed423f50ad73
myz <myz@MacBookProOfmyz.local>
2016-08-27 01:08:25 +0800

    Initial commit

---
+++ web/src/main/java/com/risker/web/contorllers/OkContorller.java
@@ -0,0 +1,54 @@
+package com.risker.web.contorllers;
+
+import com.risker.manageshared.model.User;
+import com.risker.manageshared.service.IHelloQueryService;
+import com.risker.manageshared.service.IUserQueryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+/**
+ * Created by myz on 16/8/9.
+ * 请求mapping都是小写
+ */
+@RestController
+@RequestMapping(value = "/okcontorller")
+public class OkContorller {
+
+
+    private Logger logger = LoggerFactory.getLogger(OkContorller.class);
+
+    @Autowired
+    private IHelloQueryService helloQueryService;
+    @Autowired
+    private IUserQueryService userQueryService;
+
+    @RequestMapping(value = "/ok",produces = {"text/plain;charset=utf-8"})
+    public String ok(){
+        logger.info("entry .....contorller");
+        return "你好世界";
+    }
+
+    @RequestMapping(value = {"/sayHello","/sayhello"})
+    public String sayHello(){
+
+        String result = helloQueryService.sayHello("张三");
+        return result;
+    }
+
+
+    @RequestMapping("/userlist")
+    public ModelAndView userList(Model model){
+
+        List<User> userList = userQueryService.queryAll();
+        model.addAttribute("userList",userList);
+        return new ModelAndView("userList");
+    }
+
+}
